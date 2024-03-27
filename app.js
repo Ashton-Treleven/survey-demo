d6cc16edd012696af07c056fb5ced687cb67ebbf
Treleven <500206565@fvtc.edu>
2024-03-26 19:24:57 -0500

    Fav color

--- app.js
+++ app.js
@@ -6,11 +6,16 @@
 // ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝    ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝  ╚══╝╚══╝ ╚═╝ ╚═════╝╚═╝  ╚═╝
 
 (async () => {
-    const { prompts } = require('enquirer')
+    const { prompt } = require('enquirer')
     const axios = require('axios')
 
     const questions = require('./questions.json')
 
-    
+    const url = 'https://raw.githubusercontent.com/Ashton-Treleven/survey-demo/master/questions.json'
+
+    const { data } = await axios.get(url)
+
+    const answers = await prompt(data)
+    console.log(answers)
 
 })()
